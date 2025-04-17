412db764ce1d1115a003924e8fe2a8f7e1c5b701
Bobo <mukarrammukarramov09@gmail.com>
2025-04-17 19:52:26 +0500

    add swagger

--- IT-RunCourseSecondPartAPI/obj/Debug/net9.0/IT-RunCourseSecondPartAPI.AssemblyInfo.cs
+++ IT-RunCourseSecondPartAPI/obj/Debug/net9.0/IT-RunCourseSecondPartAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IT-RunCourseSecondPartAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0e0a6aa1e0830e477fdde9eef851e3ee1a780aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("IT-RunCourseSecondPartAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IT-RunCourseSecondPartAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
