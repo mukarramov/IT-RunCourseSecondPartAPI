633f5f9f53be282f7e965d36ecdf3b1b58bd5015
Bobo <mukarrammukarramov09@gmail.com>
2025-05-15 15:31:36 +0500

    update gitignore

--- IT-RunCourseSecondPartAPI/obj/Debug/net9.0/IT-RunCourseSecondPartAPI.AssemblyInfo.cs
+++ IT-RunCourseSecondPartAPI/obj/Debug/net9.0/IT-RunCourseSecondPartAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IT-RunCourseSecondPartAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3713799412f550214d16abf4a684f571065862ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71909aa39633f230ec5d59107e3bb34969231328")]
 [assembly: System.Reflection.AssemblyProductAttribute("IT-RunCourseSecondPartAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IT-RunCourseSecondPartAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
