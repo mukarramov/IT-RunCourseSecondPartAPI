9ba4c3c8d2a321b9c5b4dd5618d229f5f21cd7fe
Bobo <mukarrammukarramov09@gmail.com>
2025-05-27 20:42:26 +0500

    added migration

---
+++ OnlineMarket/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+using IT_RunCourseSecondPartAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace IT_RunCourseSecondPartAPI.Data;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+}
--- OnlineMarket/Program.cs
+++ OnlineMarket/Program.cs
@@ -1,13 +1,18 @@
 using FluentValidation;
 using IT_RunCourseSecondPartAPI;
+using IT_RunCourseSecondPartAPI.Data;
 using IT_RunCourseSecondPartAPI.Mapper;
 using IT_RunCourseSecondPartAPI.MinimalAPI.Extensions;
 using IT_RunCourseSecondPartAPI.Validations;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.DependInjection();
 
